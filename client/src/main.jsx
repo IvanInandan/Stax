1c03b901c2e1576c185eb49aa6fbee0e4e846b10
IvanInandan <c.ivaninandan@gmail.com>
2025-12-16 18:33:10 -0800

    Setup template for FE using Zustand, Tanstack Query, and React Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Set up Tanstack Query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+// Set up react-router
+import { BrowserRouter as Router } from "react-router-dom";
+
+// // Set up global state management
+// import { Provider } from "react-redux";
+// import store from "./store";
+
+// // Import Mantine UI
+// import "@mantine/core/styles.css";
+// import { MantineProvider } from "@mantine/core";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      {/* <MantineProvider
+          defaultColorScheme="dark"
+          theme={{ primaryColor: "blue" }}
+        > */}
+      <App />
+      {/* </MantineProvider> */}
+    </Router>
+  </QueryClientProvider>
+  // </Provider>
+);
