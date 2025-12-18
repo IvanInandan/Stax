f0a7556690a6fca4774cca71af2d7c6e56cae4cd
IvanInandan <c.ivaninandan@gmail.com>
2025-12-17 18:22:06 -0800

    Import tweakcn theming and add shadcn button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <>
       <div>
         <h1>Hello</h1>
+      </div>
+
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
       </div>
     </>
   );
